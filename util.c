75e264357c0dbd02cbe89db8ca65e2c3667c7c7e
Damon <damon@petta.org>
2018-06-09 20:13:40 +0100

    fmt

--- util.c
+++ util.c
@@ -9,25 +9,25 @@
 void *
 ecalloc(size_t nmemb, size_t size)
 {
-	void *p;
+    void *p;
 
-	if (!(p = calloc(nmemb, size)))
-		perror(NULL);
-	return p;
+    if (!(p = calloc(nmemb, size)))
+        perror(NULL);
+    return p;
 }
 
 void
 die(const char *fmt, ...) {
-	va_list ap;
+    va_list ap;
 
-	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
-	va_end(ap);
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
 
-	if (fmt[0] && fmt[strlen(fmt)-1] == ':') {
-		fputc(' ', stderr);
-		perror(NULL);
-	}
+    if (fmt[0] && fmt[strlen(fmt)-1] == ':') {
+        fputc(' ', stderr);
+        perror(NULL);
+    }
 
-	exit(1);
+    exit(1);
 }
--- zoomswap.c
+++ zoomswap.c
@@ -2,9 +2,9 @@ static Client * findbefore(Client *c);
 
 Client *
 findbefore(Client *c) {
-	Client *tmp;
-	if(c == selmon->clients)
-		return NULL;
-	for(tmp = selmon->clients; tmp && tmp->next != c; tmp = tmp->next) ;
-	return tmp;
+    Client *tmp;
+    if(c == selmon->clients)
+        return NULL;
+    for(tmp = selmon->clients; tmp && tmp->next != c; tmp = tmp->next) ;
+    return tmp;
 }
